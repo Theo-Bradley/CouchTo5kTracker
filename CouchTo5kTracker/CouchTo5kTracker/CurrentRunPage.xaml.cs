9500570caedb20e954d934a98291f1ff73994413
Theo Proulx <gaminggekoman@gmail.com>
2021-06-05 22:11:18 +0100

    Completed design of homepage

---
+++ CouchTo5kTracker/CouchTo5kTracker/CurrentRunPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CouchTo5kTracker
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CurrentRunPage : ContentPage
+    {
+        public CurrentRunPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- CouchTo5kTracker/CouchTo5kTracker/HomePage.xaml.cs
+++ CouchTo5kTracker/CouchTo5kTracker/HomePage.xaml.cs
@@ -20,11 +20,10 @@ namespace CouchTo5kTracker
         public HomePage()
         {
             InitializeComponent();
-            screenWidth = (float)Xamarin.Essentials.DeviceDisplay.MainDisplayInfo.Width;
-            //float pageWidth = (float)Application.Current.MainPage.Width;
-            MainGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength((screenWidth/2) - 200, GridUnitType.Absolute)});
-            System.Diagnostics.Debug.WriteLine((screenWidth/2) - 200);
-            MainGrid.RowDefinitions.Add(new RowDefinition { Height = GridLength.Star});
+            screenWidth = (float)Xamarin.Essentials.DeviceDisplay.MainDisplayInfo.Width; //get the width of the screen in pixels
+            canvasView.WidthRequest = screenWidth; //expand the canvas to fill the width of the screen in pixels
+            canvasView.HeightRequest = screenWidth; //keep the canvas square
+            //RunGrid.RowSpacing = 0f; //Push the next run up to the next run title
         }
 
         public void SetProgress(float value) //interaction method
@@ -38,7 +37,8 @@ namespace CouchTo5kTracker
             Style = SKPaintStyle.Stroke,
             StrokeWidth = 25, //bar width
             Color = SKColors.Red, //default color
-            StrokeCap = SKStrokeCap.Round //round ends
+            StrokeCap = SKStrokeCap.Round, //round ends
+            IsAntialias = true //enable antialiasing
         };
 
         SKPaint backPaint = new SKPaint
@@ -46,7 +46,8 @@ namespace CouchTo5kTracker
             Style = SKPaintStyle.Stroke,
             StrokeWidth = 25, //bar width
             Color = SKColor.FromHsl(0, 0, 91), //light grey color
-            StrokeCap = SKStrokeCap.Round //round ends
+            StrokeCap = SKStrokeCap.Round, //round ends
+            IsAntialias = true //enable antialiasing
         };
 
         void OnCanvasViewPaintSurface(object sender, SKPaintSurfaceEventArgs args) //when canvas is meant to be rendered
@@ -54,17 +55,17 @@ namespace CouchTo5kTracker
             SKSurface surface = args.Surface; //reference to the xaml skiasharp surface
             SKCanvas canvas = surface.Canvas; //reference to the drawing canvas
             SKColor[] sweepColors = new SKColor[2];  //array of the colors to be blended by gradient
-            sweepColors[0] = SKColor.FromHsl(203, 36, 33); //initial color
-            sweepColors[1] = SKColor.FromHsl(345, 45, 48); //final color
+            sweepColors[0] = SKColor.FromHsl(356, 83, 45); //initial color
+            sweepColors[1] = SKColor.FromHsl(209, 83, 45); //final color
             canvas.Clear(); //clear the canvas of drawings
-            
 
-            SKRect rect = new SKRect(100, 100, (float)screenWidth - 150, (float)screenWidth - 150); // rectangle the bars occupy, made to fit available space
+            SKRect rect = new SKRect(100, 100, screenWidth - 100, screenWidth - 100); // rectangle the bars occupy, made to fit available space
             float startAngle = 40f; //initial angle from the bottom for the bars
-            progress = (float)SweepSlider.Value; //set the progress% to a slider value for testing
+            progress = 50; //set the progress% to 50 for testing
             float maxAngle = 360f - (2 * startAngle); //calculate hte maximum sweep angle
 
             float sweepAngle = (maxAngle) * (progress/100); //interpolate between the canvas
+
 
             canvas.Save(); //save canvas's original state
             canvas.RotateDegrees(90f, rect.MidX, rect.MidY); //rotate the canvas about the middle of the progress bar
@@ -94,6 +95,17 @@ namespace CouchTo5kTracker
         private void PrepareCanvas()
         {
             canvasView.InvalidateSurface(); //tell the canvas it needs to be redrawn
+        }
+
+        private async void TapGestureRecognizer_Tapped(object sender, EventArgs e) //Next run frame/date clicked
+        {
+            await Navigation.PushAsync(new RunsPage()); //open the RunsPage
+        }
+
+        private async void TapGestureRecognizer_Tapped_Run(object sender, EventArgs e) //start run button tapped
+        {
+            await Navigation.PushModalAsync(new CurrentRunPage()); //change TimerPage to the relevant page for this run, then a back button to PopModalAsync
+            //on clicking back have a dialogue before using PopModalAsync to prevent accidental closures: https://docs.microsoft.com/en-us/xamarin/xamarin-forms/user-interface/pop-ups
         }
     }
 }
